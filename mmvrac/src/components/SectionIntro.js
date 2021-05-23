d76c918fa2ca4b4ca0c3b2d2bb657052f8487908
Mark Huang <hhzeos@gmail.com>
2021-05-23 22:35:13 +0800

    Update

--- mmvrac/src/components/SectionIntro.js
+++ mmvrac/src/components/SectionIntro.js
@@ -14,32 +14,32 @@ const SectionIntro = () => {
                     <li className="my-2">
                         <b>Track-1 Video Question Answering </b>
 
-                        based on the dataset <a href="multi-modal-video-reasoning/#/datasets">SUTD-TrafficQA</a>, which is a comprehensive VideoQA benchmark
+                        based on the dataset <a href="https://sutdcv.github.io/multi-modal-video-reasoning/#/datasets">SUTD-TrafficQA</a>, which is a comprehensive VideoQA benchmark
                         focusing on traffic-event based video reasoning.
                         
                     </li>
                     <li className="my-2">
                         <b>Track-2 Skeleton-based Action Recognition </b>
-                        based on <a href="multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which is a comprehensive and challenging human
+                        based on <a href="https://sutdcv.github.io/multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which is a comprehensive and challenging human
                         behavior understanding dataset captured by a Unmanned Aerial Vehicle.
                     </li>
                     <li className="my-2">
                         <b>Track-3 Fisheye Video-based Action Recognition </b >
-                        based on <a href="multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which also provides video samples
+                        based on <a href="https://sutdcv.github.io/multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which also provides video samples
                         recorded by a fisheye camera, captures wide
                         angle of views yet brings challenges because of large
                         distortions in the edge areas. 
                     </li>
                     <li className="my-2">
                         <b>Track-4 Person Re-Identification </b>
-                        based on <a href="multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which also provides a person ReID track
+                        based on <a href="https://sutdcv.github.io/multi-modal-video-reasoning/#/datasets">UAV-Human</a>, which also provides a person ReID track
                         with 11,805 frames containing 618 identities, with
                         diversified viewpoints, scenarios and occlusions.
                     </li>
                 </ul>
             </div>
             <Button
-                href="multi-modal-video-reasoning/#/datasets"
+                href="https://sutdcv.github.io/multi-modal-video-reasoning/#/datasets"
                 variant="outlined"
                 color="primary"
                 className=""
