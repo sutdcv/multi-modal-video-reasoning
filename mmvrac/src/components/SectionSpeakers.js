be5de7633fc664286cad4531309f7a70edbca067
LITianjiao <sayano.lee@gmail.com>
2021-05-25 09:58:23 +0800

    Update speaker

--- mmvrac/src/components/SectionSpeakers.js
+++ mmvrac/src/components/SectionSpeakers.js
@@ -9,6 +9,7 @@ const SectionSpeakers = () => {
                 <PersonProfile name="Alex Kot" imgSrc="imgs/profiles/alex_kot.jpg" link="https://personal.ntu.edu.sg/eackot/index.html" title="" org="Rapid-Rich Object SEarch Laboratory (ROSE), Nanyang Technological University (NTU)" dim={135}/>
                 {/* <PersonProfile name="Lingyu Duan" imgSrc="imgs/profiles/lingyu_duan.jpg" link="https://eecs.pku.edu.cn/info/1459/9475.htm" title="" org="National Engineering Laboratory of Video Technology (NELVT), Peking University (PKU)" dim={135}/> */}
                 <PersonProfile name="Jiaying Liu" imgSrc="imgs/profiles/jiaying_liu.png" link="http://39.96.165.147/people/liujiaying.html" title="" org="Wangxuan Institute of Computer Technology, Peking University (PKU)" dim={135}/>
+                <PersonProfile name="Piotr Koniusz" imgSrc="imgs/profiles/pk.jpg" link="http://claret.wikidot.com/" title="" org="Data61, CSIRO" dim={135}/>
             </div>
         </div>
     )
